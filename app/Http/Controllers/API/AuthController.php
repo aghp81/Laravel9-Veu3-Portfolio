4c79f86c68f81abde031540fef35f3a62e864962
abol ghana <aghp81@gmail.com>
2022-12-23 19:07:45 +0330

    Valdator - register function

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 
 class AuthController extends Controller
@@ -12,7 +13,22 @@ class AuthController extends Controller
     public function register(Request $request){
         //vlidator
         $validator = Validator::make($request->all(),[
-
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
         ]);
+
+        if($validator->fails()){
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
     }
 }
