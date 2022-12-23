3e99e9217c3dec51521d1cb882258389304015f9
abol ghana <aghp81@gmail.com>
2022-12-23 21:04:29 +0330

    login

--- resources/js/components/admin/home/index.vue
+++ resources/js/components/admin/home/index.vue
@@ -3,4 +3,10 @@
     <div>
         <router-link to="/">Home page</router-link>
     </div>
+
+    <br>
+
+    <div>
+        <router-link to="/login">Login</router-link>
+    </div>
 </template>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -6,6 +6,9 @@ import homeAdminIndex from "../components/admin/home/index.vue"
 // pages
 import homePageIndex from "../components/pages/home/index.vue"
 
+// login
+import login from "../components/auth/login.vue"
+
 import notFound from "../components/notFound.vue"
 
 const routes = [
@@ -19,6 +22,12 @@ const routes = [
     {
         path: "/",
         component: homePageIndex
+    },
+
+    // login
+    {
+        path: "/login",
+        component: login,
     },
 
     //notFound
